b505e0c00c8a92b57835450b61b7c50663d6f652
UncontestedTopDog <William18819444345@163.com>
2017-10-20 11:19:17 +0800

    company
    2017/10/20/11/19 Commit

--- app/src/main/java/com/example/administrator/newmovie/RoundImageView.java
+++ app/src/main/java/com/example/administrator/newmovie/RoundImageView.java
@@ -29,11 +29,15 @@ import com.bumptech.glide.load.resource.bitmap.GlideBitmapDrawable;
 
 public class RoundImageView extends ImageView {
 
+    private static final int CIRCLE = 1;
+    private static final int RECTANGLE = 0;
+
     private int style;
     private int color;
     private int width;
-    private BitmapPool bitmapPool;
-    private int defaultWidth = 0 , defaultHeight = 0 ;
+    private float margin;
+    private int margincolor;
+    private int defaultWidth = 0, defaultHeight = 0;
 
     public RoundImageView(Context context) {
         this(context, null, 1);
@@ -52,42 +56,15 @@ public class RoundImageView extends ImageView {
         TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.RoundImageView);
         width = ta.getInt(R.styleable.RoundImageView_width, 1);
         color = ta.getColor(R.styleable.RoundImageView_color, Color.WHITE);
-        style = ta.getInt(R.styleable.RoundImageView_style, 1);
-        Log.i("ASDADASDA",width+"");
+        style = ta.getInt(R.styleable.RoundImageView_style, CIRCLE);
+        margin = ta.getFloat(R.styleable.RoundImageView_margin, 0);
+        margincolor = ta.getInt(R.styleable.RoundImageView_margincolor, Color.GRAY);
         ta.recycle();
-    }
-
-    private Bitmap handlePicture(Bitmap picture, int radius) {
-        Bitmap resultBitmap = null;
-        int x = 0, y = 0;
-        int bitmapsize = picture.getWidth() > picture.getHeight() ? picture.getHeight() : picture.getWidth();
-        if (picture.getWidth() > picture.getHeight())
-            x = picture.getWidth() / 2 - picture.getHeight() / 2;
-        else
-            y = picture.getHeight() / 2 - picture.getWidth() / 2;
-        picture = Bitmap.createBitmap(picture, x, y, bitmapsize, bitmapsize);
-        picture = Bitmap.createScaledBitmap(picture, radius * 2, radius * 2, false);
-        resultBitmap = Bitmap.createBitmap(bitmapsize, bitmapsize, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(resultBitmap);
-        Paint paint = new Paint();
-        //新建一个矩形区域位置为0, 0, squareBitmap.getWidth(),squareBitmap.getHeight()
-        Rect rect = new Rect(0, 0, radius * 2, radius * 2);
-        paint.setAntiAlias(true);//设置抗锯齿
-        paint.setFilterBitmap(true);//对位图进行滤波处理
-        paint.setDither(true);//设置防抖动
-        canvas.drawARGB(0, 0, 0, 0);//画背景颜色为透明
-        if (style == 1) {
-            canvas.drawCircle(radius, radius, radius, paint);
-            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        }
-        canvas.drawBitmap(picture, rect, rect, paint);
-        picture = null;
-        return resultBitmap;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        Drawable drawable = getDrawable() ;
+        Drawable drawable = getDrawable();
         //如果图片为空，直接放返回
         if (drawable == null)
             return;
@@ -96,13 +73,12 @@ public class RoundImageView extends ImageView {
             return;
         this.measure(0, 0);
         //如果是.9图也无法显示，直接返回
-        if(drawable.getClass() == NinePatchDrawable.class)
+        if (drawable.getClass() == NinePatchDrawable.class)
             return;
-        Bitmap b = null ;
-        try{
+        Bitmap b = null;
+        try {
             b = ((BitmapDrawable) drawable).getBitmap();
-        }
-        catch (ClassCastException e){
+        } catch (ClassCastException e) {
             b = ((GlideBitmapDrawable) drawable).getBitmap();
         }
         Bitmap bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
@@ -114,69 +90,86 @@ public class RoundImageView extends ImageView {
             defaultHeight = getHeight();
         }
         //半径的等于宽高中较小的一边的一半
-        int radius = (defaultHeight > defaultWidth ? defaultWidth :defaultHeight )/2;
-        Bitmap roundBitmap = handlePicture(bitmap, radius);
-        canvas.drawBitmap(roundBitmap,0,0,null);
+        int radius = (defaultHeight > defaultWidth ? defaultWidth : defaultHeight) / 2;
+        Bitmap roundBitmap = handlePicture(bitmap, radius, style);
+        canvas.drawBitmap(roundBitmap, 0, 0, null);
         Paint paint = new Paint();
         paint.setAntiAlias(true);//设置抗锯齿
         paint.setFilterBitmap(true);//对位图进行滤波处理
         paint.setDither(true);//设置防抖动
         paint.setColor(color);
         paint.setStrokeWidth(width);
-        if (style==1){
-            Path path = new Path();
-            path.moveTo(radius,0);
-            path.quadTo(0,0,0,radius);
-            path.quadTo(0,2*radius,radius,2*radius);
-            path.quadTo(2*radius,2*radius,2*radius,radius);
-            path.quadTo(2*radius,0,radius,0);
-            canvas.drawPath(path,paint);
-        }
-        else if (style==0){
-            Path path = new Path();
-            path.moveTo(0,0);
-            path.lineTo(0,2*radius);
-            path.lineTo(2*radius,2*radius);
-            path.lineTo(2*radius,0);
-            path.lineTo(0,0);
-            canvas.drawPath(path,paint);
-        }
-
-    }
-
-    /**
-     * Convert a drawable object into a Bitmap.
-     *
-     * @param drawable Drawable to extract a Bitmap from.
-     * @return A Bitmap created from the drawable parameter.
-     */
-    protected Bitmap drawableToBitmap(Drawable drawable) {
-        if (drawable == null)   // Don't do anything without a proper drawable
-            return null;
-        else if (drawable instanceof BitmapDrawable) {  // Use the getBitmap() method instead if BitmapDrawable
-            ((BitmapDrawable) drawable).getBitmap();
-        }
-
-        int intrinsicWidth = drawable.getIntrinsicWidth();
-        int intrinsicHeight = drawable.getIntrinsicHeight();
-
-        if (!(intrinsicWidth > 0 && intrinsicHeight > 0))
-            return null;
-
-        try {
-            // Create Bitmap object out of the drawable
-            Bitmap bitmap = bitmapPool.get(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888);
-            if (bitmap == null) {
-                bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888);
+        paint.setStyle(Paint.Style.STROKE);
+        if (style == CIRCLE) {
+            canvas.drawCircle(radius, radius, radius - (float) (width / 2), paint);
+            if (margin != 0) {
+                paint.setColor(margincolor);
+                paint.setStrokeWidth(margin);
+                canvas.drawCircle(radius, radius, radius - (float) (margin / 2), paint);
             }
-
-            Canvas canvas = new Canvas(bitmap);
-            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-            drawable.draw(canvas);
-            return bitmap;
-        } catch (OutOfMemoryError e) {
-            // Simply return null of failed bitmap creations
-            return null;
+        } else if (style == RECTANGLE) {
+            Path path = new Path();
+            path.moveTo(0, 0);
+            path.lineTo(0, roundBitmap.getHeight());
+            path.lineTo(roundBitmap.getWidth(), roundBitmap.getHeight());
+            path.lineTo(roundBitmap.getWidth(), 0);
+            path.lineTo(0, 0);
+            canvas.drawPath(path, paint);
+            if (margin != 0) {
+                paint.setColor(margincolor);
+                paint.setStrokeWidth(margin);
+                canvas.drawPath(path, paint);
+            }
         }
+
     }
+
+    private Bitmap handlePicture(Bitmap bitmap, int radius, int style) {
+        if (style == RECTANGLE)
+            return Bitmap.createScaledBitmap(bitmap, getWidth(), getHeight(), false);
+        Bitmap squareBitmap;
+        int x = 0, y = 0;
+        int bitmapWidth = bitmap.getWidth();
+        int bitmapHeight = bitmap.getHeight();
+        int squareWidth = 0, squareHeight = 0;
+        //以bitmap的短的边的标准，截取一个以bitmap的中心为中心的正方形
+        if (bitmapWidth > bitmapHeight) {
+            x = bitmapWidth / 2 - bitmapHeight / 2;
+            y = 0;
+            squareHeight = squareWidth = bitmapHeight;
+        } else {
+            y = bitmapHeight / 2 - bitmapWidth / 2;
+            x = 0;
+            squareHeight = squareWidth = bitmapWidth;
+        }
+        squareBitmap = Bitmap.createBitmap(bitmap, x, y, squareWidth, squareHeight);
+        //将squareBitmap缩放成diameter的大小
+        squareBitmap = Bitmap.createScaledBitmap(squareBitmap, radius * 2, radius * 2, false);
+        //新建一个bitmap resultbitmap，大小为squareBitmap的大小
+        Bitmap resultbitmap = Bitmap.createBitmap(squareBitmap.getWidth(),
+                squareBitmap.getHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(resultbitmap);
+        Paint paint = new Paint();
+        //新建一个矩形区域位置为0, 0, squareBitmap.getWidth(),squareBitmap.getHeight()
+        Rect rect = new Rect(0, 0, squareBitmap.getWidth(), squareBitmap.getHeight());
+        paint.setAntiAlias(true);//设置抗锯齿
+        paint.setFilterBitmap(true);//对位图进行滤波处理
+        paint.setDither(true);//设置防抖动
+        canvas.drawARGB(0, 0, 0, 0);//画背景颜色为透明
+        //以（squareBitmap.getWidth() / 2,squareBitmap.getHeight() / 2）为圆心，squareBitmap.getWidth() / 2为半径画圆
+        canvas.drawCircle(squareBitmap.getWidth() / 2,
+                squareBitmap.getHeight() / 2,
+                squareBitmap.getWidth() / 2,
+                paint);
+        //精髓，因为这句话，canvas原先画的透明背景圆和接下来的squareBitmap重叠部分显示出来
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        //在画布中将squareBitmap画出来
+        canvas.drawBitmap(squareBitmap, rect, rect, paint);
+        bitmap = null;
+        squareBitmap = null;
+        //将画布中的内容返回
+        return resultbitmap;
+    }
+
 }
